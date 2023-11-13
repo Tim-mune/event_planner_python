5d62324b25509074c2978db78f72aa57b891a038
timmy <neshtimo38@gmail.com>
2023-11-13 10:20:35 +0300

    change to beanie for async operations

--- planner/db/connect.py
+++ planner/db/connect.py
@@ -1,15 +1,40 @@
-from dotenv import load_dotenv, find_dotenv
-from pymongo import MongoClient
-import os
+# from dotenv import load_dotenv
+# from pymongo import MongoClient
+# import os
+
+# load_dotenv()
+# async def connect():
+#     try:
+#         client = MongoClient(os.environ.get("mongo_url"))
+#         db = client.test_db
+#         db.Users.find_one
+#         print("dabase connected")
+#         return db
+#     except Exception as err:
+#         print(err)
+
+# switch to beanie(async orm)
+from models.users import User
+from models.events import Events
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
 load_dotenv()
+import os
+
+mongo_uri = os.getenv("MONGO_URI")
 
 
 async def connect():
     try:
-        client = MongoClient(os.environ.get("mongo_url"))
-        db = client.fastapi_test
-        print("dabase connected")
-        return db
+        client = AsyncIOMotorClient(mongo_uri)
+        await init_beanie(database=client["events"], document_models=[User, Events])
+        print("database connected")
     except Exception as err:
         print(err)
+
+
+async def addUser():
+    user = User(username="tim", email="tim@gmail.com", password="1234")
+    await user.create()
